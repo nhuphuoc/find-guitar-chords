98a7fd6a459e9292ccabb1e1c6d840e6a8a3c64c
nhuphuoc.bui <nhuphuoc.bui@gmail.com>
2024-09-09 19:27:06 +0700

    open new hopamchuan and show hopam

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,29 @@
 // Lắng nghe thông điệp từ popup 
-chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
-    if (request.action === "showAlert") {
-        alert("Hello from Extension! Content: " + request.content);
-    }
+chrome.runtime.onMessage.addListener(async function (
+  request,
+  sender,
+  sendResponse
+) {
+  if (request.action === "showAlert") {
+    // alert("Hello from Extension! Content: " + request.content);
+    const xpathResult = document.evaluate(
+      '//*[@id="title"]/h1/yt-formatted-string',
+      document,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null
+    );
+
+    // Lấy giá trị innerText của phần tử
+    const element = xpathResult.singleNodeValue;
+    const value = element ? element.innerText : null;
+    const searchQuery = extractSongTitle(value) + 'hopamchuan';
+    const searchUrl =
+      "https://hopamchuan.com/search?q=" + extractSongTitle(value);
+
+    // Mở trang tìm kiếm trong tab mới
+    window.open(searchUrl, "_blank");
+  }
 }); 
 
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
